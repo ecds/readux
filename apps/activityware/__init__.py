36b07ef2a53d101f9ba1c75092c83dc4d42b1f27
Val Neekman <val@neekware.com>
2013-10-14 00:08:40 -0400

    grammer

--- apps/activityware/__init__.py
+++ apps/activityware/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Val Neekman [neekware.com]'
 __version__ = '0.0.1'
-__note__ = 'This application that tracs user activities'
+__note__ = 'This application tracks user activities'
 
 import listeners
 

0170782a9ae69255ef9db9a8d992f1cb2fb54fd7
Val Neekman <val@neekware.com>
2013-10-28 22:06:51 -0400

    rename handy scripts

---
+++ bin/development.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+from os import path
+import sys
+
+# Ever forgot (DEBUG = True) on your production server? Well no more!
+# This is a drop-in replacement of Django's (manage.py) to be used when developing / debugging.
+# Never, Ever use it on a production site
+################################################################################
+
+# Bootstrap Django
+for item in ['apps', 'www']:
+    sys.path.insert(0, path.abspath(path.join(path.dirname(__file__), '../{}'.format(item))))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'settings.develop')
+from django.core.management import execute_from_command_line
+execute_from_command_line(sys.argv)

4792f75d82a7d21abb1f34328a4ef3debe8db4f1
rlskoeser <rebecca.s.koeser@emory.edu>
2014-05-20 17:44:27 -0400

    add total/currently displayed number of volumes to collection page; unit tests for collection view [#67572876]

--- readux/collection/tests.py
+++ readux/collection/tests.py
@@ -1,10 +1,11 @@
-from django.core.urlresolvers import reverse
 from django.test import TestCase
-from mock import patch
+from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator
+from mock import patch, NonCallableMock
 
 from eulfedora.server import Repository
 from readux.collection.models import Collection
-from readux.books.models import Volume
+from readux.books.models import Volume, SolrVolume
 
 class CollectionTest(TestCase):
 
@@ -16,9 +17,9 @@ class CollectionTest(TestCase):
         self.assertEqual('Civil War Literature', coll.short_label)
 
 
+@patch('readux.collection.views.solr_interface')
 class CollectionViewsTest(TestCase):
 
-    @patch('readux.collection.views.solr_interface')
     def test_browse(self, mocksolr_interface):
         mocksolr = mocksolr_interface.return_value
 
@@ -63,6 +64,81 @@ class CollectionViewsTest(TestCase):
             msg_prefix='page should link to collection view for %(pid)s' % solr_result[1])
         self.assertContains(response, '5 volumes',
             msg_prefix='volume count should be displayed and properly pluralized')
+
+
+    @patch('readux.collection.views.Repository')
+    @patch('readux.collection.views.Paginator', spec=Paginator)
+    def test_browse(self, mockpaginator, mockrepo, mocksolr_interface):
+        mocksolr = mocksolr_interface.return_value
+        view_url = reverse('collection:view', kwargs={'pid': 'coll'})
+
+        # simulate 404
+        mockcoll = NonCallableMock()
+        mockrepo.return_value.get_object.return_value = mockcoll
+        # - doesn't exist
+        mockcoll.exists = False
+        response = self.client.get(view_url)
+        expected, got = 404, response.status_code
+        self.assertEqual(expected, got,
+            'expected status code %s but got %s for view collection when object doesn\'t exist' % \
+            (expected, got))
+        # - exists but is the wrong type
+        mockcoll.exists = True
+        mockcoll.has_requisite_content_models = False
+        response = self.client.get(view_url)
+        expected, got = 404, response.status_code
+        self.assertEqual(expected, got,
+            'expected status code %s but got %s for view collection when object has wrong cmodels' % \
+            (expected, got))
+
+        # simulate valid fedora object
+        mockcoll.has_requisite_content_models = True
+        mockcoll.short_label = 'Yellowbacks'
+        mockcoll.pid = 'coll:1'
+        # simulate sunburnt's fluid interface
+        mocksolr.query.return_value = mocksolr.query
+        for method in ['query', 'sort_by', 'results_as']:
+            getattr(mocksolr.query, method).return_value = mocksolr.query
+
+        # set up mock results for display on template
+        solr_result = [
+            SolrVolume(**{'pid': 'vol:1', 'title': 'Asodecoan'}),
+            SolrVolume(**{'pid': 'vol:2', 'title': 'Sugar Crop of Lousiana', 'label': 'ocm123_V.2'}),
+        ]
+
+        # - using a noncallable for the pagination result that is used in the template
+        # because passing callables into django templates does weird things
+        mockpage = NonCallableMock()
+        mockpaginator.return_value.page.return_value = mockpage
+        mockpage.object_list = solr_result
+        mockpage.has_other_pages = False
+        mockpage.paginator.count = 2
+        mockpage.paginator.page_range = [1]
+
+        response = self.client.get(view_url)
+        # print response
+        # inspect solr query
+        mocksolr.query.assert_called_with(content_model=Volume.VOLUME_CONTENT_MODEL,
+                                          collection_id=mockcoll.pid)
+        mocksolr.query.sort_by.assert_any_call('title_exact')
+        mocksolr.query.sort_by.assert_any_call('label')
+        mocksolr.query.results_as.assert_called_with(SolrVolume)
+
+        # inspect html result
+        self.assertContains(response, mockcoll.short_label,
+            msg_prefix='collection short label should be displayed')
+        self.assertContains(response,
+            '<title>Readux | Collections | %s</title>' % mockcoll.short_label,
+            html=True,
+            msg_prefix='collection label should be included in html title')
+        self.assertContains(response, '2 volumes in this collection',
+            msg_prefix='total count of volumes in the collection should be displayed')
+        self.assertContains(response, solr_result[0]['title'],
+            msg_prefix='volume title %(title)s should be displayed' % solr_result[0])
+        self.assertContains(response, solr_result[1]['title'],
+            msg_prefix='volume title %(title)s should be displayed' % solr_result[1])
+        self.assertContains(response, '(V.2)',
+            msg_prefix='volume number should be displayed when present')
 
 
 
--- readux/settings.py
+++ readux/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'eulfedora',
     'eultheme',
     'readux.collection',

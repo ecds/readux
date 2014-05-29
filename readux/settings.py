751c8b83b8e46cbe1900a4c12e35daed87986ae4
rlskoeser <rebecca.s.koeser@emory.edu>
2014-05-29 13:16:18 -0400

    add config for static root directory

--- readux/settings.py
+++ readux/settings.py
@@ -96,6 +96,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 # Additional locations of static files
 STATICFILES_DIRS = [
     # Put strings here, like "/home/html/static" or "C:/www/django/static".

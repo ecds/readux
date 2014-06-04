df3a7c286b80bcd2953b91e1b19bd72154023d85
rlskoeser <rebecca.s.koeser@emory.edu>
2014-06-04 14:31:47 -0400

    use python-requests with sunburnt instead of httplib2 [#71777576]

--- readux/utils.py
+++ readux/utils.py
@@ -1,5 +1,5 @@
 import os
-import httplib2
+import requests
 from urlparse import urlparse
 from sunburnt import sunburnt
 
@@ -13,26 +13,26 @@ def solr_interface():
     one is set.  Additionally, if an **HTTP_PROXY** is set in the
     environment, it will be configured.
     '''
-    http_opts = {}
+
+    session = requests.Session()
+
     if hasattr(settings, 'SOLR_CA_CERT_PATH'):
-        http_opts['ca_certs'] = settings.SOLR_CA_CERT_PATH
+        session.cert = settings.SOLR_CA_CERT_PATH
+        session.verify = True
     if getattr(settings, 'SOLR_DISABLE_CERT_CHECK', False):
-        http_opts['disable_ssl_certificate_validation'] = True
+        session.verify = False
 
-    # use http proxy if set in ENV
+    # configure requests to use http proxy if one is set in ENV
     http_proxy = os.getenv('HTTP_PROXY', None)
-    solr_url = urlparse(settings.SOLR_SERVER_URL)
-    # NOTE: using Squid with httplib2 requires no-tunneling proxy option
-    # - non-tunnel proxy does not work with https
-    if http_proxy and solr_url.scheme == 'http':
+    if http_proxy:
         parsed_proxy = urlparse(http_proxy)
-        proxy_info = httplib2.ProxyInfo(proxy_type=httplib2.socks.PROXY_TYPE_HTTP_NO_TUNNEL,
-                                        proxy_host=parsed_proxy.hostname,
-                                        proxy_port=parsed_proxy.port)
-        http_opts['proxy_info'] = proxy_info
-    http = httplib2.Http(**http_opts)
+        session.proxies = {
+           parsed_proxy.scheme: http_proxy   # i.e., 'http': 'hostname:3128'
+        }
 
-    solr_opts = {'http_connection': http}
+    # pass in the constructed requests session as the connection to be used
+    # when making requests of solr
+    solr_opts = {'http_connection': session}
     # since we have the schema available, don't bother requesting it
     # from solr every time we initialize a new connection
     if hasattr(settings, 'SOLR_SCHEMA'):

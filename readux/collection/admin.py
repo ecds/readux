587fc03a61d2d5ffcf7eeb6342edca555f2f40d9
rlskoeser <rebecca.s.koeser@emory.edu>
2014-07-03 12:06:08 -0400

    db model for collection images and logic to access image from collection object or solr result [#74305084]

--- readux/collection/admin.py
+++ readux/collection/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from readux.collection.models import CollectionImage
+
+
+class CollectionImageAdmin(admin.ModelAdmin):
+    list_display = ('collection_label', 'cover', 'banner')
+
+admin.site.register(CollectionImage, CollectionImageAdmin)
--- readux/collection/models.py
+++ readux/collection/models.py
@@ -1,9 +1,38 @@
 import re
+from UserDict import UserDict
+from django.db import models
+from django_image_tools.models import Image
 
 from eulcm.models.collection.v1_0 import Collection as Collectionv1_0
+from readux.utils import solr_interface
 
 
-class Collection(Collectionv1_0):
+class BaseCollection(object):
+    '''Common functionality for :class:`Collection` and :class:`SolrCollection`'''
+
+    @property
+    def images(self):
+        'associated :class:`CollectionImage` if there is one for this pid'
+        try:
+            return CollectionImage.objects.filter(collection=self.pid).get()
+        except CollectionImage.DoesNotExist:
+            pass
+
+    @property
+    def cover(self):
+        'cover image for associated :class:`CollectionImage`, if available'
+        if self.images:
+            return self.images.cover
+
+    @property
+    def banner(self):
+        'cover image for associated :class:`CollectionImage`, if available'
+        if self.images:
+            return self.images.banner
+
+
+
+class Collection(Collectionv1_0, BaseCollection):
     '''Fedora Collection Object.  Extends
     :class:`~eulcm.models.collection.v1_0.Collection`.
     '''
@@ -27,3 +56,50 @@ class Collection(Collectionv1_0):
         data['title'] = self.short_label
         return data
 
+
+class SolrCollection(UserDict, BaseCollection):
+    '''Extension of :class:`~UserDict.UserDict` for use with Solr results
+    for volume-specific content.  Extends :class:`BaseCollection` for common
+    collection logic (specifically access to related images).
+    '''
+
+    def __init__(self, **kwargs):
+        # sunburnt passes fields as kwargs; userdict wants them as a dict
+        UserDict.__init__(self, kwargs)
+
+    @property
+    def pid(self):
+        'object pid'
+        return self.data.get('pid')
+
+
+def collection_choices():
+    'collection choices (pid and title) for :attr:`CollectionImage.collection`'
+    solr = solr_interface()
+    results = solr.query(content_model=Collection.COLLECTION_CONTENT_MODEL) \
+          .filter(owner='LSDI-project') \
+          .sort_by('title_exact')  \
+          .field_limit(['pid', 'title'])
+
+    choices = [(r['pid'], r['title']) for r in results]
+    return choices
+
+
+class CollectionImage(models.Model):
+    # NOTE: collection field  should be selected from fedora lsdi collections;
+    # stores the pid, but displays the collection label to the user
+    collection = models.CharField(max_length=255, unique=True, choices=collection_choices())
+    cover = models.ForeignKey(Image, related_name='coverimage_set')
+    banner = models.ForeignKey(Image, blank=True, null=True,
+                               related_name='bannerimage_set')
+
+    @property
+    def collection_label(self):
+        # get collection label from solr via pid
+        solr = solr_interface()
+        results = solr.query(pid=self.collection).field_limit('title')
+        if results:
+            return results[0]['title']
+        else:
+            # return collection pid as fallback if lookup title fails
+            return self.collection

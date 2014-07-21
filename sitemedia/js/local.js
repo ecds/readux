79446536a375976d001f9b690ac5a5525c7f376f
Kevin Glover <hkglover@gmail.com>
2014-07-21 11:16:15 -0400

    Added back button support for view toggles
    [#74956790]
    
    Added hashchange function to local.js to handle back button when
    toggling.

--- sitemedia/js/local.js
+++ sitemedia/js/local.js
@@ -1,3 +1,56 @@
+/*** deprecated browser function ***/
+(function() {
+    var matched, browser;
+
+    // Use of jQuery.browser is frowned upon.
+    // More details: http://api.jquery.com/jQuery.browser
+    // jQuery.uaMatch maintained for back-compat
+    jQuery.uaMatch = function( ua ) {
+        ua = ua.toLowerCase();
+
+        var match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
+            /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+            /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+            /(msie) ([\w.]+)/.exec( ua ) ||
+            ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
+            [];
+
+        return {
+            browser: match[ 1 ] || "",
+            version: match[ 2 ] || "0"
+        };
+    };
+
+    matched = jQuery.uaMatch( navigator.userAgent );
+    browser = {};
+
+    if ( matched.browser ) {
+        browser[ matched.browser ] = true;
+        browser.version = matched.version;
+    }
+
+    // Chrome is Webkit, but Webkit is also Safari.
+    if ( browser.chrome ) {
+        browser.webkit = true;
+    } else if ( browser.webkit ) {
+        browser.safari = true;
+    }
+
+    jQuery.browser = browser;
+})();
+
+/*
+ * jQuery hashchange event - v1.3 - 7/21/2010
+ * http://benalman.com/projects/jquery-hashchange-plugin/
+ * 
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+(function($,e,b){var c="hashchange",h=document,f,g=$.event.special,i=h.documentMode,d="on"+c in e&&(i===b||i>7);function a(j){j=j||location.href;return"#"+j.replace(/^[^#]*#?(.*)$/,"$1")}$.fn[c]=function(j){return j?this.bind(c,j):this.trigger(c)};$.fn[c].delay=50;g[c]=$.extend(g[c],{setup:function(){if(d){return false}$(f.start)},teardown:function(){if(d){return false}$(f.stop)}});f=(function(){var j={},p,m=a(),k=function(q){return q},l=k,o=k;j.start=function(){p||n()};j.stop=function(){p&&clearTimeout(p);p=b};function n(){var r=a(),q=o(m);if(r!==m){l(m=r,q);$(e).trigger(c)}else{if(q!==m){location.href=location.href.replace(/#.*/,"")+q}}p=setTimeout(n,$.fn[c].delay)}$.browser.msie&&!d&&(function(){var q,r;j.start=function(){if(!q){r=$.fn[c].src;r=r&&r+a();q=$('<iframe tabindex="-1" title="empty"/>').hide().one("load",function(){r||l(a());n()}).attr("src",r||"javascript:0").insertAfter("body")[0].contentWindow;h.onpropertychange=function(){try{if(event.propertyName==="title"){q.document.title=h.title}}catch(s){}}}};j.stop=k;o=function(){return a(q.location.href)};l=function(v,s){var u=q.document,t=$.fn[c].domain;if(v!==s){u.title=h.title;u.open();t&&u.write('<script>document.domain="'+t+'"<\/script>');u.close();q.location.hash=v}}})();return j})()})(jQuery,this);
+
+
+
 $(document).ready(function() {
     // collection browse cover view
     var $container = $('#cover-list');
@@ -12,25 +65,44 @@ $(document).ready(function() {
       transitionDuration:'0.4s'
     });
 
+    var $toggle_link = $('#view-toggle a')
+
     // collection browse toggle view modes
-    $('#view-toggle a').click(function() {
-        $('#view-toggle a').removeClass('active');
-        var show_filter = '.coverlist-toggle > .item-' + $(this).attr('id');
-        $('.coverlist-toggle > div[class^="item-"]').hide().removeClass('hidden');
-        $(show_filter).show();
-        $(this).addClass('active');
-        var href= $(this).attr('href') ||'';
-        window.location.hash = href;
-    });
 
-    var hash = window.location.hash; 
+    if($toggle_link.length>0){
 
-    if(hash){
-      if(hash.indexOf('list-view')>0){
-        $("a#list").click();
+      $toggle_link.click(function() {
+          $('#view-toggle a').removeClass('active');
+          var show_filter = '.coverlist-toggle > .item-' + $(this).attr('id');
+          $('.coverlist-toggle > div[class^="item-"]').hide().removeClass('hidden');
+          $(show_filter).show();
+          $(this).addClass('active');
+          var href= $(this).attr('href') ||'#';
+          window.location.hash = href;
+      });
+
+      console.log(window.location.hash)
+      if(window.location.hash==''){
+        var default_hash = $('#view-toggle a.active').attr('href');
+        window.location.hash = default_hash;
       }
-      else if(hash.indexOf('covers-view')>0){
-        $("a#covers").click();
-      }
+
+      $(window).hashchange( function(){
+        var hash = window.location.hash; 
+        if(hash){
+          if(hash.indexOf('list-view')>0){
+            $("a#list").click();
+          }
+          else if(hash.indexOf('covers-view')>0){
+            $("a#covers").click();
+          }
+        }
+      });
+
+      $(window).hashchange();
     }
 });
+
+
+
+

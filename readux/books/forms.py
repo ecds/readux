e244dc5b92d326225e2d65263b780ceebf59274f
rlskoeser <rebecca.s.koeser@emory.edu>
2014-08-21 13:11:43 -0400

    move search terms logic to search form for re-use

--- readux/books/forms.py
+++ readux/books/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from eulcommon.searchutil import search_terms
 
 class BookSearch(forms.Form):
     '''Form for searching books.'''
@@ -6,4 +7,11 @@ class BookSearch(forms.Form):
     keyword = forms.CharField(required=True, label='Keyword',
             help_text='Search for content by keywords or exact phrase (use quotes).</br>' +
                       'Wildcards <b>*</b> and <b>?</b> are supported.',
-          error_messages={'required': 'Please enter one or more search terms'})
+            error_messages={'required': 'Please enter one or more search terms'})
+
+    def search_terms(self):
+        '''Get a list of keywords and phrases from the keyword input field,
+        using :meth:`eulcommon.searchutil.search_terms`.  Assumes that the form
+        has already been validated and cleaned_data is available.'''
+        # get list of keywords and phrases
+        return search_terms(self.cleaned_data['keyword'])

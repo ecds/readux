9448c60ee01bbd3533cb5169c1b532dd033566a0
rlskoeser <rebecca.s.koeser@emory.edu>
2015-02-03 19:44:49 -0500

    update import script with better error handling and actual page ingest logic
    [#86124554]

--- readux/books/management/commands/import_volume.py
+++ readux/books/management/commands/import_volume.py
@@ -21,7 +21,7 @@ import time
 
 from django.core.management.base import BaseCommand, CommandError
 from eulfedora.server import RequestFailed
-from eulxml.xmlmap import load_xmlobject_from_file
+from eulxml.xmlmap import load_xmlobject_from_file, load_xmlobject_from_string
 from eulxml.xmlmap.dc import DublinCore
 
 from readux.fedora import ManagementRepository
@@ -140,6 +140,7 @@ class Command(BaseCommand):
 
         # all pieces are available, so proceed with ingest
 
+
         # construct book and ingest
         if verbosity > self.v_normal:
             self.stdout.write('Creating book object with marxml %s' % files['marcxml'])
@@ -152,72 +153,107 @@ class Command(BaseCommand):
         except XMLSyntaxError as err:
             raise CommandError('Failed to load %s as xml: %s' % (files['dc'], err))
 
-        book = repo.get_object(type=Book)
-        # set book label to ocm number from the marc
-        book.label = marcxml.ocm_number
-        if verbosity > self.v_normal:
-            self.stdout.write('Book label %s' % book.label)
+        # look for book by ocm number first, in case a previous ingest failed
+        book_pids = Book.pids_by_label(marcxml.ocm_number)
+        # error if we find more than one
+        if len(book_pids) > 1:
+            raise CommandError('Multiple books exist with label %s. Please correct this first.' \
+                                % marcxml.ocm_number)
 
-        # associate with collection
-        if collection is not None:
-            book.collection = collection
+        # if we find exactly one, use that instead of creating a new book
+        elif len(book_pids) == 1:
+            book = repo.get_object(book_pids[0], type=Book)
+            if verbosity >= self.v_normal:
+                self.stdout.write('Using existing book %s' % book.pid)
+
+        # otherwise, ingest new book
+        else:
+            book = repo.get_object(type=Book)
+            # set book label to ocm number from the marc
+            book.label = marcxml.ocm_number
             if verbosity > self.v_normal:
-                self.stdout.write('Associating with collection %s' % collection.short_label)
-        book.marcxml.content = marcxml
-        # NOTE: import checksum can't be used because xml may be serialized differently
-        # book.marcxml.checksum = checksums['marcxml']
-        book.dc.content = dcxml
-        # NOTE: import checksum can't be used because DC is modified to add ARK
-        # book.dc.checksum = checksums['dc']
+                self.stdout.write('Book label %s' % book.label)
 
-        # save; bail if error
-        if not dry_run:
-            try:
-                saved = book.save('ingest')
-                if not saved:
-                    raise CommandError('Failed to ingest book into repository')
-            except RequestFailed as err:
-                raise CommandError('Error ingesting book: %s' % err)
+            # associate with collection
+            if collection is not None:
+                book.collection = collection
+                if verbosity > self.v_normal:
+                    self.stdout.write('Associating with collection %s' % collection.short_label)
+            book.marcxml.content = marcxml
+            # NOTE: import checksum can't be used because xml may be serialized differently
+            # book.marcxml.checksum = checksums['marcxml']
+            book.dc.content = dcxml
+            # NOTE: import checksum can't be used because DC is modified to add ARK
+            # book.dc.checksum = checksums['dc']
 
-
-        # construct volume (v1.1), associate with book, and ingest
-        if verbosity > self.v_normal:
-            self.stdout.write('Creating volume with %s' % files['pdf'])
-        with open(files['pdf']) as pdf_file:
-            vol = repo.get_object(type=VolumeV1_1)
-            # set volume label to ocm number from the marc + volume number
-            # for consistency with lsdi content, use ocm_v# notation
-            # V.0 indicates single-volume book
-            vol.label = '%s_V.0' % marcxml.ocm_number
-            # set pdf content
-            vol.pdf.content = pdf_file
-            vol.pdf.checksum = checksums['pdf']
-            # set relation to parent book object
-            vol.book = book
-            # minimal DC metadata derived from book metadata
-            vol.dc.content.title = book.dc.content.title
-            for t in book.dc.content.type_list:
-                vol.dc.content.type_list.append(t)
-            vol.dc.content.format = book.dc.content.format
-            vol.dc.content.language = book.dc.content.language
-            vol.dc.content.rights = book.dc.content.rights
-
+            # save; bail if error
             if not dry_run:
                 try:
-                    saved = vol.save('ingest')
+                    saved = book.save('ingest')
                     if not saved:
-                        # NOTE: possibly, if this fails, we should deactivate the book object
-                        # but will leave that to manual processing for now
-                        raise CommandError('Failed to ingest volume into repository')
-
+                        raise CommandError('Failed to ingest book into repository')
+                    if verbosity >= self.v_normal:
+                        self.stdout.write('Successfully ingested book %s' \
+                                    % book.pid)
                 except RequestFailed as err:
-                    raise CommandError('Error ingesting volume: %s' % err)
+                    raise CommandError('Error ingesting book: %s' % err)
+
+        # in case of pre-existing book object, check for existing volume
+        if book.volume_set:
+            if len(book.volume_set) > 1:
+                raise CommandError('Book %s has multiple volumes; import not supported' \
+                    % book.pid)
+            else:
+                # use existing volume object
+                vol = book.volume_set[0]
+                if verbosity >= self.v_normal:
+                    self.stdout.write('Using existing volume %s' % vol.pid)
+
+        # otherwise, create new volume object
+        else:
+            # construct volume (v1.1), associate with book, and ingest
+            if verbosity > self.v_normal:
+                self.stdout.write('Creating volume with %s' % files['pdf'])
+            with open(files['pdf']) as pdf_file:
+                vol = repo.get_object(type=VolumeV1_1)
+                # set volume label to ocm number from the marc + volume number
+                # for consistency with lsdi content, use ocm_v# notation
+                # V.0 indicates single-volume book
+                vol.label = '%s_V.0' % marcxml.ocm_number
+                # set pdf content
+                vol.pdf.content = pdf_file
+                vol.pdf.checksum = checksums['pdf']
+                # set relation to parent book object
+                vol.book = book
+                # minimal DC metadata derived from book metadata
+                vol.dc.content.title = book.dc.content.title
+                for t in book.dc.content.type_list:
+                    vol.dc.content.type_list.append(t)
+                vol.dc.content.format = book.dc.content.format
+                vol.dc.content.language = book.dc.content.language
+                vol.dc.content.rights = book.dc.content.rights
+
+                if not dry_run:
+                    try:
+                        saved = vol.save('ingest')
+                        if not saved:
+                            # NOTE: possibly, if this fails, we should deactivate the book object
+                            # but will leave that to manual processing for now
+                            raise CommandError('Failed to ingest volume into repository')
+                        else:
+                            if verbosity >= self.v_normal:
+                                self.stdout.write('Successfully ingested volume %s' \
+                                    % vol.pid)
+                    except RequestFailed as err:
+                        raise CommandError('Error ingesting volume: %s' % err)
 
 
-        if not dry_run:
-            self.stdout.write('Successfully ingested book %s and volume %s' % \
-                (book.pid, vol.pid))
+        #### page import
 
+        # if volume has existing pages, bail
+        if len(vol.pages):
+            raise CommandError('Volume %s already has %s page%s' % \
+                (vol.pid, len(vol.pages), '' if len(vol.pages) == 1 else 's'))
 
         # should page import happen here?
         # - identify numeric jp2/jpf files in the bag and get total count
@@ -259,6 +295,10 @@ class Command(BaseCommand):
         #         raise CommandError('No OCR xml page present for %s (expected %s)' % \
         #             (imgfile, ocrfile))
 
+        # pre-generate empty xml in case we need it to force eulfedora to not
+        # create ocr datastream when no ocr is present
+        emptyxml = load_xmlobject_from_string('<empty/>')
+
         # iterate through page images and put into fedora
         pageindex = 1
         for imgfile in image_files:
@@ -267,12 +307,7 @@ class Command(BaseCommand):
             # path is relative to bag root dir
             img_filename = os.path.join(path, imgfile)
 
-            img_content = None
-            ocr_content = None
-
-            try:
-                img_content = open(img_filename)
-
+            with open(img_filename) as img_content:
                 page = repo.get_object(type=PageV1_1)
                 # set page label
                 page.label = '%s page %d' % (vol.label, pageindex)
@@ -294,8 +329,7 @@ class Command(BaseCommand):
                 ocrfile = '%s.xml' % basefile
 
                 if ocrfile in payload_files:
-                    ocr_content = open(os.path.join(path, ocrfile))
-                    page.ocr.content = ocr_content
+                    page.ocr.content = load_xmlobject_from_file(os.path.join(path, ocrfile))
                     # NOTE: can't use MD5 from bag because XML may be
                     # serialized differently when sent to Fedora
                     # (unless we treat as file instead of xml...)
@@ -304,15 +338,27 @@ class Command(BaseCommand):
                     if verbosity > self.v_normal:
                         print 'Setting OCR for Page from %s' % ocrfile
 
+                else:
+                    # warn but do not error if ocr xml is not found
+                    self.stdout.write('Warning: no OCR xml found for %s' % imgfile)
+                    # explicitly set xml content to empty so eulfedora doesn't
+                    # attempt to bootstrap & ingest (and error)
+                    page.ocr.content = emptyxml
+
                 if not dry_run:
-                    saved = page.save()
+                    try:
+                        # for now, if any page ingest errors, bail out
+                        # (unclear what would cause load to fail midway)
 
-                # TODO: catch & handle fedora error here
+                        saved = page.save()
 
-            finally:
-                img_content.close()
-                if ocr_content is not None:
-                    ocr_content.close()
+
+                        if not saved:
+                            raise CommandError('Failed to ingest page %d into repository' \
+                                % pageindex)
+
+                    except RequestFailed as err:
+                        raise CommandError('Error ingesting page %d: %s' % (pageindex, err))
 
             # set first page as primary image for the volume
             if not dry_run and pageindex == 1:
@@ -321,8 +367,9 @@ class Command(BaseCommand):
 
             # increase page index for next page
             pageindex += 1
-            # TEMPORARY: stop here for testing purposes
-            break
+
+        if verbosity >= self.v_normal:
+            self.stdout.write('Created %d pages' % pageindex)
 
 
 

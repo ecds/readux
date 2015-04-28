c05df81d6cf6a6c201a532b4f213b838b053c0c1
rlskoeser <rebecca.s.koeser@emory.edu>
2015-04-28 19:39:01 -0400

    Updated ldap user migration
    Handle case where auth_user was never created because
    the AUTH_USER_MODEL was configured as the emory_ldap
    user model from the beginning.

--- readux/collection/migrations/0003_emoryldap_user_to_auth_user.py
+++ readux/collection/migrations/0003_emoryldap_user_to_auth_user.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from django.conf import settings
 
 # fields common to emory ldap user and auth user
 common_fields = ['username', 'password', 'first_name', 'last_name',
@@ -22,24 +21,72 @@ def copy_user_to_user(a, b):
         b.user_permissions.add(perm)
 
 
+emoryldap_extra_fields = ['phone', 'dept_num', 'full_name', 'title',
+    'employee_num', 'subdept_code', 'hr_id']
+
 def migrate_ldap_users(apps, schema_editor):
     # get ldap user and standard auth user models
     ldap_user = apps.get_model('emory_ldap', 'EmoryLDAPUser')
     auth_user = apps.get_model('auth', 'user')
-    # for each ldap user, make sure there is an equivalent auth user
-    for ldapuser in ldap_user.objects.all():
-        user, created = auth_user.objects.get_or_create(username=ldapuser.username)
-        copy_user_to_user(ldapuser, user)
-        user.save()
-        ldapuser.delete()
+
+    # depending on how the database was created, auth_user may not exist
+    if 'auth_user' not in schema_editor.connection.introspection.table_names():
+        print 'Moving emoryldap_user table to auth_user.'
+        print 'emory_ldap should be removed from installed apps.'
+
+        # if auth_user doesn't exist, rename the emory_ldap table to
+        # auth_user and drop the unnecessary fields
+        ldap_fields = ['id', 'password']
+        # remove fields in ldap user not in auth user
+        for field in ldap_user._meta.fields:
+            ldap_fields.append(field.name)
+            if field.name in emoryldap_extra_fields:
+                schema_editor.remove_field(ldap_user, field)
+
+        # rename the table
+        schema_editor.alter_db_table(auth_user, ldap_user._meta.db_table,
+            auth_user._meta.db_table)
+
+        # add fields in auth user not in emory ldap
+        for field in auth_user._meta.fields:
+            if field.name == 'id':
+                continue
+            if field not in ldap_fields:
+                schema_editor.add_field(auth_user, field)
+
+    else:
+    # if both tables exist, copy user information over
+
+        # for each ldap user, make sure there is an equivalent auth user
+        for ldapuser in ldap_user.objects.all():
+            user, created = auth_user.objects.get_or_create(username=ldapuser.username)
+            copy_user_to_user(ldapuser, user)
+            user.save()
+            ldapuser.delete()
 
 def unmigrate_ldap_users(apps, schema_editor):
+    # depending on how the database was created, auth_user may not exist
     ldap_user = apps.get_model('emory_ldap', 'EmoryLDAPUser')
     auth_user = apps.get_model('auth', 'user')
-    for user in auth_user.objects.all():
-        ldapuser, created = ldap_user.objects.get_or_create(username=user.username)
-        copy_user_to_user(user, ldapuser)
-        ldapuser.save()
+
+    # if we migrated by moving the table, then it won't exist, so undo the same way
+    if 'emory_ldap_emoryldapuser' not in schema_editor.connection.introspection.table_names():
+
+        schema_editor.alter_db_table(ldap_user, auth_user._meta.db_table,
+            ldap_user._meta.db_table)
+
+        # add extra fields ldap_user expects
+        for field in ldap_user._meta.fields:
+            if field.name in emoryldap_extra_fields:
+                schema_editor.add_field(ldap_user, field)
+
+        # don't worry about removing fields in auth but not in ldap
+    else:
+        # if both tables exist, just duplicate user objects
+        for user in auth_user.objects.all():
+            ldapuser, created = ldap_user.objects.get_or_create(username=user.username)
+            copy_user_to_user(user, ldapuser)
+            ldapuser.save()
 
 
 class Migration(migrations.Migration):

fe99704eb40b80228e7151cca520faa879c0f315
Kevin Glover <hkglover@gmail.com>
2015-04-29 16:32:39 -0400

    Static files reference
    [#85804158]
    
    - Updated static files references to use django static tag
    - Fixed javascript bug with ark_uri

--- readux/urls.py
+++ readux/urls.py
@@ -53,3 +53,4 @@ urlpatterns = patterns('',
 
 if settings.DEV_ENV:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

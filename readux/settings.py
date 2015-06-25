52f0d309ae3ea9d07ee292e2d3a3e12edb0f58da
Kevin Glover <hkglover@gmail.com>
2015-06-25 15:52:04 -0400

    Include Downtime from EULtheme
    [#96819396]
    
    - Updated eultheme req
    - Added dependencies, middleware, and processor
    - Added customized maintenance page

--- readux/settings.py
+++ readux/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     # should be removed in the next version
     'eullocal.django.emory_ldap',
     'eultheme',
+    'downtime',
     'widget_tweaks',
     'readux.collection',
     'readux.books',
@@ -47,6 +48,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'eultheme.middleware.DownpageMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'social.apps.django_app.middleware.SocialAuthExceptionMiddleware'
@@ -67,6 +69,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'social.apps.django_app.context_processors.login_redirect',
     "eultheme.context_processors.template_settings",
     "eultheme.context_processors.site_path",
+    "eultheme.context_processors.downtime_context",
     "readux.context_extras",  # include app version, backend names
     "readux.books.context_processors.book_search",  # book search form
 )

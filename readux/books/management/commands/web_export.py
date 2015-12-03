146a2217fba0614d5f03e6a8648ced9613dc2cb8
rlskoeser <rebecca.s.koeser@emory.edu>
2015-12-03 15:38:20 -0500

    Update web export manage command to generate static/jekyll site

--- readux/books/management/commands/web_export.py
+++ readux/books/management/commands/web_export.py
@@ -1,7 +1,8 @@
 from eulfedora.server import Repository
 from django.core.management.base import BaseCommand
+import shutil
 
-from readux.books import export
+from readux.books import annotate, export
 from readux.books.models import Volume
 
 
@@ -11,9 +12,18 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('pid', nargs='+', type=str)
+        parser.add_argument('--static', action='store_true', default=False,
+            help='Generate built (static) site instead of jekyll site')
 
     def handle(self, *args, **options):
+        print args
         repo = Repository()
         for pid in options['pid']:
-            vol = repo.get_object('emory:4ckk0', type=Volume)
-            export.static_website(vol)
+            vol = repo.get_object(pid, type=Volume)
+            tei = annotate.annotated_tei(vol.generate_volume_tei(),
+                vol.annotations())
+            zipfile = export.website(vol, tei, static=options['static'])
+            zipfilename = '%s-annotated-site.zip' % pid
+            shutil.copyfile(zipfile.name, zipfilename)
+
+            print 'Export for %s complete, zipfile is %s' % (vol.noid, zipfilename)

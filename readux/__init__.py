e6e26ea3df1c84cd84958b063133f7b81beebf80
rlskoeser <rebecca.s.koeser@emory.edu>
2015-12-18 12:42:00 -0500

    Bump version to 1.3.7

--- readux/__init__.py
+++ readux/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = (1, 3, 6, None)
+__version_info__ = (1, 3, 7, None)
 
 # Dot-connect all but the last. Last is dash-connected if not None.
 __version__ = '.'.join([str(i) for i in __version_info__[:-1]])

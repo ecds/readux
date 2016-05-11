4aeb3d09fbcf338d84aa99bf402fc58e4c0f0def
rlskoeser <rebecca.s.koeser@emory.edu>
2016-05-11 11:54:43 -0400

    Tweak debug-panel loading so it will not error without dev eulfedora

--- readux/settings.py
+++ readux/settings.py
@@ -209,6 +209,9 @@ if django_nose is not None:
 if DEBUG or DEV_ENV:
     try:
         import debug_toolbar
+        # import to ensure debug panel is available before configuring
+        # (not yet in released version of eulfedora)
+        from eulfedora import debug_panel
         INSTALLED_APPS.append('debug_toolbar')
     except ImportError:
         pass

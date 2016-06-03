07d29f0e62204b118cdecf34a25ecce12b5de727
Graham Hukill <ghukill@gmail.com>
2016-06-03 17:00:19 +0000

    typo in iiif.py, addition of loris image id suffix, and extension of tei xsl

--- readux/books/models.py
+++ readux/books/models.py
@@ -120,6 +120,7 @@ class IIIFImage(iiif.IIIFImageClient):
 
     api_endpoint = settings.IIIF_API_ENDPOINT
     image_id_prefix = getattr(settings, 'IIIF_ID_PREFIX', '')
+    image_id_suffix = getattr(settings, 'IIIF_ID_SUFFIX', '')
     pid = None
     long_side = 'height'
 
@@ -139,8 +140,8 @@ class IIIFImage(iiif.IIIFImageClient):
         return copy
 
     def get_image_id(self):
-        'image id, based on fedora pid and configured prefix'
-        return '%s%s' % (self.image_id_prefix, self.pid)
+        'image id, based on fedora pid, configured prefix, and optional suffix'
+        return '%s%s%s' % (self.image_id_prefix, self.pid, self.image_id_suffix)
 
     # NOTE: using long edge instead of specifying both with exact
     # results in cleaner urls/filenams (no !), and more reliable result
@@ -474,7 +475,7 @@ class PageV1_1(Page):
         save the result as tei datastream content.'''
         # check to make sure generated TEI is valid
         pagetei = self.generate_tei()
-        if not pageteidoc.schema_valid():
+        if not pagetei.schema_valid():
             raise Exception('TEI is not valid according to configured schema')
         # load as tei should maybe happen here instead of in generate
         self.tei.content = pagetei

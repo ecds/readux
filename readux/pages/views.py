6d2f823f4c63f1e5abc4ac8be500d2da6747a5fd
rlskoeser <rebecca.s.koeser@emory.edu>
2016-09-20 14:16:01 -0400

    Tweak page base template to include twitter/og metadata title
    For site index, uses first lead content in plain text as the description
    or subtitle for the site.
    
    [#126694979]

--- readux/pages/views.py
+++ readux/pages/views.py
@@ -4,4 +4,14 @@ from feincms.views.cbv.views import Handler
 class SiteIndex(Handler):
     # extend default feincms view handler so it can be bound to the
     # index url and referenced as a named route
-    pass
+
+    def get_context_data(self, **kwargs):
+        ctxt_data = super(SiteIndex, self).get_context_data()
+        lead = self.object.content.lead
+        # if lead content is present, pass in to the template
+        # as a tagline or description for embedded metadata
+        if lead:
+            ctxt_data.update({
+                'tagline': self.object.content.lead[0].render()
+            })
+        return ctxt_data

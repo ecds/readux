a8c0cacb081f2680d89413566a37c9b7bd15e43c
Val Neekman <val@neekware.com>
2018-10-17 13:41:00 -0400

    add config & readux to pythonpath, make readux a module

--- manage.py
+++ manage.py
@@ -4,6 +4,12 @@ import sys
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+    for item in ['config', 'readux']:
+        sys.path.insert(0, os.path.abspath(os.path.join(
+            os.path.dirname(__file__), os.pardir, os.pardir, item)))
+
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

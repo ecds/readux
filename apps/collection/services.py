d9abe229f5d11a362a790808ef13efaa0564c958
Val Neekman <val@neekware.com>
2018-10-23 12:07:04 -0400

    add get_collection by url

--- apps/collection/services.py
+++ apps/collection/services.py
@@ -1,7 +1,41 @@
 import requests
 import json
 
+from django.conf import settings
+from . import defaults as defs
+
+
+def get_collections(url):
+    """
+    Given a url, this function returns a dictonary of all collections.
+    """
+    collections = []
+    try:
+        resp = requests.get(url, timeout=defs.HTTP_REQUEST_TIMEOUT)
+    except requests.exceptions.Timeout as err:
+        if (settings.DEBUG):
+            print('Connection timeoutout for {}'.format(url))
+        return collections
+    except Exception as err:
+        if (settings.DEBUG):
+            print('Connection failed for {}'.format(str(err)))
+        return collections
+    
+    if resp.status_code != 200:
+        if (settings.DEBUG):
+            print('Connection failed status {}'.format(resp.status_code))
+        return collections
+
+    try:
+        jsonDict = resp.json()
+        collections = jsonDict['collections']
+    except KeyError as err:
+        if (settings.DEBUG):
+            print('No collections for {}'.format(url))
+
+    return collections
+
 def get_all_collections():
-    url = 'http://ryanfb.github.io/iiif-universe/iiif-universe.json' 
-    r = requests.get(url)
-    return r.json()['collections']
+    """ Returns all top level collections """
+    return get_collections(defs.IIIF_UNIVERSE_COLLECTION_URL)
+
--- apps/collection/views.py
+++ apps/collection/views.py
@@ -8,5 +8,4 @@ class CollectionList(TemplateView):
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
     context['collections'] = collection.services.get_all_collections()
-    print(len(context['collections']))
     return context

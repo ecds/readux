d5e3a3bd198ff820ba137a01c4779ccec9e17605
Jay Varner <jayvarner@gmail.com>
2018-11-06 12:06:28 -0500

    Rorginize apps, create iiif models ane serializers.

--- apps/profiles/__init__.py
+++
@@ -1,9 +0,0 @@
-##
-# @license
-# Copyright Neekware Inc. All Rights Reserved.
-#
-# Use of this source code is governed by an MIT-style license that can be
-# found in the LICENSE file at http://neekware.com/license/MIT.html
-###
-
-default_app_config = 'profiles.apps.ProfilesConfig'

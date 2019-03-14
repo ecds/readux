094b4ac1126082d8f2b6b6002e632ca1ab88027f
Joanna <joanna@jcmundy.wireless.emory.edu>
2019-03-14 16:00:53 -0400

    added field for IIIF Image Server

--- apps/iiif/canvases/admin.py
+++ apps/iiif/canvases/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from apps.iiif.canvases.models import Canvas
+from apps.iiif.canvases.models import Canvas, IServer
 
 class CanvasAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'pid', 'height', 'width', 'position', 'manifest', 'label')
     
+class IServerAdmin(admin.ModelAdmin):
+    list_display = ('IIIF_IMAGE_SERVER_BASE',)
+    
 admin.site.register(Canvas, CanvasAdmin)
+admin.site.register(IServer, IServerAdmin)

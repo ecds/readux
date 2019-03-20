7a488432cc86a8e428bb4de246ee9ad5b70e8602
Sara Palmer <sara.palmer@emory.edu>
2019-03-20 14:25:03 -0400

    wire wagtail

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,14 @@
 from django.conf import settings
 from django.conf.urls import url
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.core import urls as wagtail_urls
 
 urlpatterns = [
     re_path(r'^cms/', include(wagtailadmin_urls)),

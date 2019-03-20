4bfbd9b166d21464567b959593f989109ef56501
Joanna <joanna@JCMUNDY.local>
2019-03-19 17:55:10 -0700

    working on canvas import

--- apps/iiif/canvases/admin.py
+++ apps/iiif/canvases/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 
+from import_export import resources, fields
+from import_export.admin import ImportExportModelAdmin
+from import_export.widgets import ForeignKeyWidget, ManyToManyWidget
 from apps.iiif.canvases.models import Canvas, IServer
+from apps.iiif.manifests.models import Manifest
 
-class CanvasAdmin(admin.ModelAdmin):
+class CanvasResource(resources.ModelResource):
+    manifestid = fields.Field(
+        column_name='manifest',
+        attribute='manifest',
+        widget=ForeignKeyWidget(Manifest, 'pid'))
+    class Meta:
+        model = Canvas
+        import_id_fields = ['uuid']
+        fields = ('uuid', 'pid', 'position','height','width', 'IIIF_IMAGE_SERVER_BASE','manifestid')
+
+class CanvasAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    resource_class = CanvasResource
+    pass     
     list_display = ('uuid', 'pid', 'height', 'width', 'position', 'manifest', 'label')
     
 class IServerAdmin(admin.ModelAdmin):

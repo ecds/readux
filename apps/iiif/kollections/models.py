4c8f76ebc73d997c964b60a1933f2c5add428e1a
Sara Palmer <sara.palmer@emory.edu>
2019-04-03 15:31:07 -0400

    Added orderable featured items to home page

--- apps/iiif/kollections/models.py
+++ apps/iiif/kollections/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
-from django.template.defaultfilters import slugify 
+from django.template.defaultfilters import slugify
 import uuid
 
 class Collection(models.Model):
@@ -10,6 +10,7 @@ class Collection(models.Model):
     pid = models.CharField(max_length=255)
     metadata = JSONField(null=True)
     upload = models.FileField(upload_to='uploads/', null=True)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return self.label

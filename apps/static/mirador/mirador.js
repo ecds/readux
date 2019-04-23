a21a37325575c27edef985f6082008e951413e52
Joanna <joanna@jcmundy.library.emory.ddns>
2019-04-23 16:39:58 -0400

    fix quotation marks

--- apps/static/mirador/mirador.js
+++ apps/static/mirador/mirador.js
@@ -41894,7 +41894,8 @@ return /******/ (function(modules) { // webpackBootstrap
         var facebookContainer = document.getElementById("facebookpage");
         var face = document.createElement('a');
 //         var faceText = document.createTextNode('http://www.facebook.com/sharer.php?s=100&p[url]=' + content3.content + '/page/' + page + &p[images][0] + content1.content + '/' + page + '/full/600,/0/default.jpg');
-        face.href = "'http://www.facebook.com/sharer.php?s=100&p[url]=' + content3.content + '/page/' + page + &p[images][0] + content1.content + '/' + page + '/full/600,/0/default.jpg'";
+        var facehref='http://www.facebook.com/sharer.php?s=100&p[url]=' + content3.content + '/page/' + page + '&p[images][0]' + content1.content + '/' + page + '/full/600,/0/default.jpg';
+        face.href=facehref;
         face.title = 'Share with Facebook';
 //         face.appendChild(faceText);
         facebookContainer.innerHTML='<img src="https://www.cabq.gov/culturalservices/biopark/images/share-on-facebook.png/@@images/image.png" title="Facebook" alt="Facebook" ></a>';

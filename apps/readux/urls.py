b2805c143d992f693ee8a770ac2de9ba1338d8ec
Joanna <joanna@jcmundy.library.emory.ddns>
2019-07-11 14:47:01 -0400

    vol to volume

--- apps/readux/urls.py
+++ apps/readux/urls.py
@@ -5,13 +5,13 @@ from . import views
 # from .views import PageRedirectView
 
 urlpatterns = [
-  path('col/', views.CollectionsList.as_view(), name='home' ),
-  path('col/<collection>/', views.CollectionDetail.as_view(), name="collection" ),
-  path('vol/<volume>', views.VolumeDetail.as_view(), name='volume' ),
+  path('collection/', views.CollectionsList.as_view(), name='home' ),
+  path('collection/<collection>/', views.CollectionDetail.as_view(), name="collection" ),
+  path('volume/<volume>', views.VolumeDetail.as_view(), name='volume' ),
   # url for page altered to prevent conflict with Wagtail
   # TODO: find another way to resolve this conflict
-  path('vol/<volume>/page/<page>', views.PageDetail.as_view(), name='page' ),
+  path('volume/<volume>/page/<page>', views.PageDetail.as_view(), name='page' ),
 #   path('col/<collection>/vol/<volume>/page/', RedirectView.as_view(pattern_name='page'), name='page 1' ),
 #   path('col/<collection>/vol/<volume>/page/', PageRedirectView.as_view(), name='page 1' ),
-  path('vol/<volume>/export', views.ExportOptions.as_view(), name='export' ),
+  path('volume/<volume>/export', views.ExportOptions.as_view(), name='export' ),
 ]

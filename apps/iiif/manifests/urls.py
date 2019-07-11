f581c7a40b42e240f66aaefda0d5e740deae9ebd
Joanna <joanna@jcmundy.library.emory.ddns>
2019-07-11 14:47:01 -0400

    vol to volume

--- apps/iiif/manifests/urls.py
+++ apps/iiif/manifests/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
   path('iiif/<version>/<pid>/manifest', views.ManifestDetail.as_view(), name="ManifestRender"),
   path('iiif/<version>/<pid>/export', views.ManifestExport.as_view(), name="ManifestExport"),
-  path('vol/<volume>/citation.ris', views.ManifestRis.as_view(), name='ris' ),
+  path('volume/<volume>/citation.ris', views.ManifestRis.as_view(), name='ris' ),
 ]

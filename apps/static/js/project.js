d804ea524554a151e37b854196a0f2f3b959c549
Yang Li <yang@truman.edu>
2019-09-25 16:34:40 -0400

    add active link support in navbar

--- apps/static/js/project.js
+++ apps/static/js/project.js
@@ -4,3 +4,15 @@
     $bannerInfo.on('click',function(){
         $(this).toggleClass('collasped');
     });
+
+
+    // Iterate through menu items and highlight the current page if it matches the URL
+    $.each($(".rx-nav-item"), function(index, navItem) {
+        var activePage = window.location.pathname.replace(
+          /^\/([^\/]*).*$/,
+          "$1"
+        );
+        if (navItem.href.includes(activePage)) {
+            navItem.classList.add("uk-active");
+        }
+    });

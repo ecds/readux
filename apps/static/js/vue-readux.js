eb93767505e95deca060c664aab6d625e59d896d
Yang Li <yang@truman.edu>
2019-10-01 14:22:48 -0400

    add a book volume image component

--- apps/static/js/vue-readux.js
+++ apps/static/js/vue-readux.js
@@ -1,10 +1,59 @@
+Vue.component("v-volume-image", {
+  props: ["srcFirst", "srcLast", "isStartingPage", "isLastItem", "volumeLabel"],
+  template: `
+    <div class="rx-card-image-container uk-text-center rx-volume-na" v-cloak v-if="!hasImage">
+      <span class="rx-item-na" uk-icon="icon: image; ratio: 3"></span>
+      <p>Volume is being added or cover is not available.</p>
+    </div>
+    <img v-else="hasImage" :src="imgSrc" :alt="imgAlt">
+  `,
+  data: function() {
+    return {
+      hasImage: true, // visibility of image from src
+      imgSrc: ""
+    };
+  },
+  mounted() {
+    var vm = this;
+    var tester = new Image();
+    tester.addEventListener("load", function() {
+      vm.hasImage = true;
+    });
+    tester.addEventListener("error", function() {
+      vm.hasImage = false;
+    });
+
+    if (this.isStartingPage == 1) {
+      tester.src = this.srcFirst;
+      this.imgSrc = this.srcFirst;
+    } else if (this.isLastItem == "yes") {
+      tester.src = this.srcLast;
+      this.imgSrc = this.srcLast;
+    } else {
+      this.hasImage = false;
+    }
+  },
+  computed: {
+    imgAlt() {
+      return `First page of ${this.volumeLabel}`; 
+    },
+    isFirst() {
+      return this.isStartingPage == 1;
+    },
+    isLast() {
+      return (this.isLastItem == "yes")
+    }
+  }
+});
+
 new Vue({
   el: "#v-readux",
   delimiters: ["[[", "]]"],
   data: {
     options: ["title", "author", "date published", "date added"],
     searchPrefix: "?sort=",
-    currentSelection: null
+    currentSelection: null,
+    itemNotFound: false,
   },
   methods: {
     sortBy: function(selection) {

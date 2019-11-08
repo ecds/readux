78b3a32a4da62c3f779b51f42c78cfec2330155c
Jay Varner <jayvarner@gmail.com>
2019-11-08 09:13:19 -0500

    Update another URL

--- apps/static/mirador/mirador.js
+++ apps/static/mirador/mirador.js
@@ -42551,6 +42551,8 @@ return /******/ (function(modules) { // webpackBootstrap
     this.annotationsList = [];
     Vue.set(readux.$refs["rx-url-canvas"], "url", options.uri); // readux is the vue instance
     Vue.set(readux.$refs["rx-url-canvas"], "label", "Stable Canvas"); // readux is the vue instance
+    Vue.set(readux.$refs["rx-url-stable-page"], "url", window.location.href); // readux is the vue instance
+    Vue.set(readux.$refs["rx-url-stable-page"], "label", "Stable Page"); // readux is the vue instance
     var linkContainer = document.getElementById("myLink");
     var link = document.createElement('a');
     var linkText = document.createTextNode(options.uri);

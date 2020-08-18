5d5a36e576c1c4e880e7d8b6491c76a1c5cd40a1
Joanna <joanna@JCMUNDY.local>
2020-08-18 15:53:04 -0400

    templatetag for sort and pagination of collection

---
+++ apps/readux/templatetags/url_replace.py
@@ -0,0 +1,10 @@
+from urllib.parse import urlencode
+from django import template
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def url_replace(context, **kwargs):
+    query = context['request'].GET.copy()
+    query.update(kwargs)
+    return query.urlencode()

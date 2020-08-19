a10aae5838f1e84336640b6e90ade3ac268bdf18
Joanna <joanna@JCMUNDY.local>
2020-08-19 11:42:46 -0400

    change on_delete to DO_NOTHING

--- apps/iiif/manifests/models.py
+++ apps/iiif/manifests/models.py
@@ -78,7 +78,7 @@ class Manifest(ClusterableModel):
     objects = ManifestManager()
     start_canvas = models.ForeignKey(
         'canvases.Canvas',
-        on_delete=models.SET_NULL,
+        on_delete=models.DO_NOTHING,
         related_name='start_canvas',
         blank=True,
         null=True

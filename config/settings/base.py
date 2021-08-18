37e25ca783cc9bb5d3607a3f5f58f07c18dc338b
minman28 <max.inman@emory.edu>
2021-08-18 15:30:26 -0400

    adding Bootstrap

--- config/settings/base.py
+++ config/settings/base.py
@@ -188,7 +188,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
     str(APPS_DIR.path('static')),
-]
+    ]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',

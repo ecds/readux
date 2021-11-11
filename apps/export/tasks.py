4a3ce1f8d9faa2fba885bc0531c80dd03b20d8e0
Jay Varner <jayvarner@gmail.com>
2021-11-11 08:48:58 -0500

    Move celery module

--- apps/export/tasks.py
+++ apps/export/tasks.py
@@ -9,9 +9,7 @@ from .export import JekyllSiteExport
 
 LOGGER = logging.getLogger(__name__)
 
-app = Celery('apps.ingest', result_extended=True)
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app = Celery('apps.readux', result_extended=True)
 
 @app.task(name='github_export', autoretry_for=(Exception,), retry_backoff=True, max_retries=20)
 def github_export_task(

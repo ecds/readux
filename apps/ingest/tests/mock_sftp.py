34bb5aa96e92ebb82505649f4deb128fb7496513
Jay Varner <jayvarner@gmail.com>
2022-03-30 08:57:12 -0400

    Use nodename for mock SFTP server

--- apps/ingest/tests/mock_sftp.py
+++ apps/ingest/tests/mock_sftp.py
@@ -17,7 +17,7 @@ class MockSFTP:
     def __init__(self):
         self.key_file = os.path.join(tempfile.gettempdir(), 'readux_test_sshkey')
         self.port = 3373
-        self.host = 'localhost'
+        self.host = os.uname().nodename
         self.server = None
 
         if not os.path.exists(self.key_file):

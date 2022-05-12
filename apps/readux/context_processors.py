f77250e5dd6c901a34e1dc1fb9c1cbb015091d56
Jay Varner <jayvarner@gmail.com>
2022-05-12 12:20:05 -0400

    Don't get Git info in production

--- apps/readux/context_processors.py
+++ apps/readux/context_processors.py
@@ -6,10 +6,17 @@ from . import __version__
 def current_version(request=None):
     repo = Repo(settings.ROOT_DIR.path())
 
-    return {
+    version_info = {
         'DJANGO_ENV': environ['DJANGO_ENV'],
-        'APP_VERSION': __version__,
-        'BRANCH': repo.active_branch.name,
-        'COMMIT': repo.active_branch.commit.hexsha,
-        'COMMIT_DATE': repo.active_branch.commit.committed_datetime.strftime("%m/%d/%Y, %H:%M:%S")
+        'APP_VERSION': __version__
     }
+
+    if environ['DJANGO_ENV'] == 'production':
+        return version_info
+    else:
+        return {
+            **version_info,
+            'BRANCH': repo.active_branch.name,
+            'COMMIT': repo.active_branch.commit.hexsha,
+            'COMMIT_DATE': repo.active_branch.commit.committed_datetime.strftime("%m/%d/%Y, %H:%M:%S")
+        }

32d5c3ca9daa48917d9fb454571e9bc0e1204788
jcmundy jcmundy <jcmundy@emory.edu>
2022-08-24 18:19:07 -0400

    fixes resaving of thumbnail and header filenames

--- apps/iiif/kollections/models.py
+++ apps/iiif/kollections/models.py
@@ -95,48 +95,53 @@ class Collection(IiifBase):
         # If height is higher we resize vertically, if not we resize horizontally
         size = (400, 500)
         image = Image.open(self.thumbnail)
-        # Get current and desired ratio for the images
-        img_ratio = image.size[0] / float(image.size[1])
-        ratio = size[0] / float(size[1])
-        #The image is scaled/cropped vertically or horizontally depending on the ratio
-        if ratio > img_ratio:
-            image = image.resize(
-                (int(size[0]), int(size[0] * image.size[1] / image.size[0])),
-                Image.Resampling.LANCZOS)
-            # Crop in the top, middle or bottom
-            box = (0, (image.size[1] - size[1]) / 2, image.size[0], (image.size[1] + size[1]) / 2)
-            image = image.crop(box)
-        elif ratio < img_ratio:
-            imageratio = (int(size[1] * image.size[0] / image.size[1]), int(size[1]))
-            image = image.resize(imageratio, Image.Resampling.LANCZOS)
-            box = ((image.size[0] - size[0]) / 2, 0, (image.size[0] + size[0]) / 2, image.size[1])
-            image = image.crop(box)
+
+        # We need to test that this if else works correctly so that new files can be uploaded and existing files are not renamed.
+        if 'thumbnails/' in self.thumbnail.name:
+            return True
         else:
-            image = image.resize((size[0], size[1]), Image.Resampling.LANCZOS)
-        # If the scale is the same, we do not need to crop
-        thumb_name, thumb_extension = os.path.splitext(self.thumbnail.name)
-        thumb_extension = thumb_extension.lower()
+            # Get current and desired ratio for the images
+            img_ratio = image.size[0] / float(image.size[1])
+            ratio = size[0] / float(size[1])
+            #The image is scaled/cropped vertically or horizontally depending on the ratio
+            if ratio > img_ratio:
+                image = image.resize(
+                    (int(size[0]), int(size[0] * image.size[1] / image.size[0])),
+                    Image.Resampling.LANCZOS)
+                # Crop in the top, middle or bottom
+                box = (0, (image.size[1] - size[1]) / 2, image.size[0], (image.size[1] + size[1]) / 2)
+                image = image.crop(box)
+            elif ratio < img_ratio:
+                imageratio = (int(size[1] * image.size[0] / image.size[1]), int(size[1]))
+                image = image.resize(imageratio, Image.Resampling.LANCZOS)
+                box = ((image.size[0] - size[0]) / 2, 0, (image.size[0] + size[0]) / 2, image.size[1])
+                image = image.crop(box)
+            else:
+                image = image.resize((size[0], size[1]), Image.Resampling.LANCZOS)
+            # If the scale is the same, we do not need to crop
+            thumb_name, thumb_extension = os.path.splitext(self.thumbnail.name)
+            thumb_extension = thumb_extension.lower()
 
-        thumb_filename = thumb_name + '_thumb' + thumb_extension
+            thumb_filename = thumb_name + '_thumb' + thumb_extension
 
-        if thumb_extension in ['.jpg', '.jpeg']:
-            file_type = 'JPEG'
-        elif thumb_extension == '.gif':
-            file_type = 'GIF'
-        elif thumb_extension == '.png':
-            file_type = 'PNG'
-        else:
-            return False    # Unrecognized file type
+            if thumb_extension in ['.jpg', '.jpeg']:
+                file_type = 'JPEG'
+            elif thumb_extension == '.gif':
+                file_type = 'GIF'
+            elif thumb_extension == '.png':
+                file_type = 'PNG'
+            else:
+                return False    # Unrecognized file type
 
-        # Save thumbnail to in-memory file as StringIO
-        temp_thumb = BytesIO()
-        image.save(temp_thumb, file_type)
-        temp_thumb.seek(0)
+            # Save thumbnail to in-memory file as StringIO
+            temp_thumb = BytesIO()
+            image.save(temp_thumb, file_type)
+            temp_thumb.seek(0)
 
-        # set save=False, otherwise it will run in an infinite loop
-        self.thumbnail.save(thumb_filename, ContentFile(temp_thumb.read()), save=False)
-        temp_thumb.close()
-        return True
+            # set save=False, otherwise it will run in an infinite loop
+            self.thumbnail.save(thumb_filename, ContentFile(temp_thumb.read()), save=False)
+            temp_thumb.close()
+            return True
 
     def __make_header(self):
         """Private method to generate a thumbnail for the collection.
@@ -147,62 +152,67 @@ class Collection(IiifBase):
         # If height is higher we resize vertically, if not we resize horizontally
         sizebanner = (1200, 300)
         forcrop = Image.open(self.header)
-        # Get current and desired ratio for the images
-        img_ratio_banner = forcrop.size[0] / float(forcrop.size[1])
-        ratio_banner = sizebanner[0] / float(sizebanner[1])
-        #The image is scaled/cropped vertically or horizontally depending on the ratio
-        if ratio_banner > img_ratio_banner: #then it needs to be shorter
-            forcrop = forcrop.resize(
-                (int(sizebanner[0]), int(sizebanner[0] * forcrop.size[1] / forcrop.size[0])),
-                Image.Resampling.LANCZOS)
-            (width, height) = forcrop.size
-            # pylint: disable = invalid-name
-            x = (0)
-            y = (height - sizebanner[1]) / 2
-            w = width
-            h = (height + sizebanner[1]) / 2
-            # pylint: enable = invalid-name
 
-            box = (x, y, w, h)
-            cropped_image = forcrop.crop(box)
-            # Crop in the top, middle or bottom
-        elif ratio_banner < img_ratio_banner: #then it needs to be narrower
-            imagebannerratio = (int(sizebanner[1] * forcrop.size[0] / forcrop.size[1]), int(sizebanner[1]))
-            forcrop = forcrop.resize(imagebannerratio, Image.Resampling.LANCZOS)
-            (width, height) = forcrop.size
-            # pylint: disable = invalid-name
-            x = (width - sizebanner[0]) / 2 #crop from middle
-            y = (0)
-            w = (width + sizebanner[0]) / 2
-            h = height
-            # pylint: enable = invalid-name
-
-            box = (x, y, w, h)
-            cropped_image = forcrop.crop(box)
+        # We need to test that this if else works correctly so that new files can be uploaded and existing files are not renamed.
+        if 'headers/' in self.header.name:
+            return True
         else:
-            cropped_image = forcrop.resize((sizebanner[0], sizebanner[1]), Image.Resampling.LANCZOS)
-        # If the scale is the same, we do not need to crop
-        thename, theextension = os.path.splitext(self.header.name)
-        theextension = theextension.lower()
+            # Get current and desired ratio for the images
+            img_ratio_banner = forcrop.size[0] / float(forcrop.size[1])
+            ratio_banner = sizebanner[0] / float(sizebanner[1])
+            #The image is scaled/cropped vertically or horizontally depending on the ratio
+            if ratio_banner > img_ratio_banner: #then it needs to be shorter
+                forcrop = forcrop.resize(
+                    (int(sizebanner[0]), int(sizebanner[0] * forcrop.size[1] / forcrop.size[0])),
+                    Image.Resampling.LANCZOS)
+                (width, height) = forcrop.size
+                # pylint: disable = invalid-name
+                x = (0)
+                y = (height - sizebanner[1]) / 2
+                w = width
+                h = (height + sizebanner[1]) / 2
+                # pylint: enable = invalid-name
 
-        thefilename = thename + '_header' + theextension
+                box = (x, y, w, h)
+                cropped_image = forcrop.crop(box)
+                # Crop in the top, middle or bottom
+            elif ratio_banner < img_ratio_banner: #then it needs to be narrower
+                imagebannerratio = (int(sizebanner[1] * forcrop.size[0] / forcrop.size[1]), int(sizebanner[1]))
+                forcrop = forcrop.resize(imagebannerratio, Image.Resampling.LANCZOS)
+                (width, height) = forcrop.size
+                # pylint: disable = invalid-name
+                x = (width - sizebanner[0]) / 2 #crop from middle
+                y = (0)
+                w = (width + sizebanner[0]) / 2
+                h = height
+                # pylint: enable = invalid-name
 
-        if theextension in ['.jpg', '.jpeg']:
-            file_type = 'JPEG'
-        elif theextension == '.gif':
-            file_type = 'GIF'
-        elif theextension == '.png':
-            file_type = 'PNG'
-        else:
-            return False    # Unrecognized file type
+                box = (x, y, w, h)
+                cropped_image = forcrop.crop(box)
+            else:
+                cropped_image = forcrop.resize((sizebanner[0], sizebanner[1]), Image.Resampling.LANCZOS)
+            # If the scale is the same, we do not need to crop
+            thename, theextension = os.path.splitext(self.header.name)
+            theextension = theextension.lower()
 
-        # Save header to in-memory file as StringIO
-        temp_header = BytesIO()
-        cropped_image.save(temp_header, file_type)
-        temp_header.seek(0)
+            thefilename = thename + '_header' + theextension
 
-        # set save=False, otherwise it will run in an infinite loop
-        self.header.save(thefilename, ContentFile(temp_header.read()), save=False)
-        temp_header.close()
+            if theextension in ['.jpg', '.jpeg']:
+                file_type = 'JPEG'
+            elif theextension == '.gif':
+                file_type = 'GIF'
+            elif theextension == '.png':
+                file_type = 'PNG'
+            else:
+                return False    # Unrecognized file type
 
-        return True
+            # Save header to in-memory file as StringIO
+            temp_header = BytesIO()
+            cropped_image.save(temp_header, file_type)
+            temp_header.seek(0)
+
+            # set save=False, otherwise it will run in an infinite loop
+            self.header.save(thefilename, ContentFile(temp_header.read()), save=False)
+            temp_header.close()
+
+            return True

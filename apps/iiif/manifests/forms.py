45767d0927b38db09462d703400a0ac392d20d7a
Ben Silverman <ben@performantsoftware.com>
2024-03-27 11:56:41 -0400

    Allow bulk manifest metadata update via CSV

--- apps/iiif/manifests/forms.py
+++ apps/iiif/manifests/forms.py
@@ -1,9 +1,14 @@
 """Django Forms for export."""
+import csv
+from io import StringIO
 import logging
 from django import forms
 from django.contrib.admin import site as admin_site, widgets
-from .models import Language, Manifest
-from ..canvases.models import Canvas
+from django.core.validators import FileExtensionValidator
+
+from apps.iiif.manifests.models import Manifest
+from apps.iiif.canvases.models import Canvas
+from apps.ingest.services import normalize_header
 
 LOGGER = logging.getLogger(__name__)
 
@@ -49,3 +54,42 @@ class ManifestsCollectionsForm(forms.ModelForm):
                self.instance._meta.get_field('collections').remote_field,
                admin_site,
         )
+
+class ManifestCSVImportForm(forms.Form):
+    """Form to import a CSV and update the metadata for multiple manifests"""
+
+    csv_file = forms.FileField(
+        required=True,
+        validators=[FileExtensionValidator(allowed_extensions=['csv'])],
+        label="CSV File",
+        help_text="""
+            <p>Provide a CSV with a <strong>pid</strong> column, whose value in each row must match
+            the PID of an existing volume. Additional columns will be used to update the volume's
+            metadata.</p>
+            <p>Columns matching <strong>Manifest</strong> model field names will update those
+            fields directly, and any additional columns will be used to populate the volume's
+            <strong>metadata</strong> JSON field.</p>
+        """,
+    )
+
+    def clean(self):
+        # check csv has pid column
+        super().clean()
+        csv_file = self.cleaned_data.get('csv_file')
+        if csv_file:
+            reader = csv.DictReader(
+                normalize_header(
+                    StringIO(csv_file.read().decode('utf-8'))
+                ),
+            )
+            if 'pid' not in reader.fieldnames:
+                self.add_error(
+                    'metadata_spreadsheet',
+                    forms.ValidationError(
+                        """Spreadsheet must have pid column. Check to ensure there
+                        are no stray characters in the header row."""
+                    ),
+                )
+            # return back to start of file so we can read again
+            csv_file.seek(0, 0)
+        return self.cleaned_data

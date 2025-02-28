d27187c8f7a81dc6992747f79a9698cd4f85f495
Jay Varner <jayvarner@gmail.com>
2025-02-28 09:32:56 -0500

    Serialize V3 manifest metadata

--- apps/iiif/serializers/v3/manifest.py
+++ apps/iiif/serializers/v3/manifest.py
@@ -1,58 +1,57 @@
 """Module for serializing IIIF Annotation Lists"""
+
 import json
-from datetime import datetime
-from django.core.serializers.base import SerializerDoesNotExist
-from django.core.serializers import serialize
+from django.core.serializers import serialize, deserialize
 from apps.iiif.canvases.models import Canvas
 from apps.iiif.serializers.base import Serializer as JSONSerializer
+
 
 class Serializer(JSONSerializer):
     """
     Convert a queryset to IIIF Manifest
     """
+
     def _init_options(self):
         super()._init_options()
-        self.current_user = self.json_kwargs.pop('current_user', None)
+        self.current_user = self.json_kwargs.pop("current_user", None)
 
     def get_dump_object(self, obj):
         try:
-            thumbnail = f'{obj.image_server.server_base}/{obj.start_canvas.resource}'
+            thumbnail = f"{obj.image_server.server_base}/{obj.start_canvas.resource}"
         except (Canvas.MultipleObjectsReturned, Canvas.DoesNotExist):
-            thumbnail = f'{obj.image_server.server_base}/{obj.canvas_set.all().first().resource}'
+            thumbnail = f"{obj.image_server.server_base}/{obj.canvas_set.all().first().resource}"
 
         data = {
-            '@context': 'http://iiif.io/api/presentation/3/context.json',
-            'id': f'{obj.v3_baseurl}/manifest',
-            'type': 'Manifest',
-            'label': obj.label,
-            'metadata': {},
-            'summary': { 'none': [obj.summary] },
-            'thumbnail': [
+            "@context": "http://iiif.io/api/presentation/3/context.json",
+            "id": f"{obj.v3_baseurl}/manifest",
+            "type": "Manifest",
+            "label": obj.label,
+            "metadata": obj.metadata,
+            "summary": {"none": [obj.summary]},
+            "thumbnail": [
                 {
-                'id': f'{thumbnail}/full/600,/0/default.jpg',
-                'type': 'Image',
-                'format': 'image/jpeg',
-                'service': [
-                    {
-                    'id': thumbnail,
-                    'type': 'ImageService3',
-                    'profile': 'level1'
-                    }
-                ]
+                    "id": f"{thumbnail}/full/600,/0/default.jpg",
+                    "type": "Image",
+                    "format": "image/jpeg",
+                    "service": [
+                        {"id": thumbnail, "type": "ImageService3", "profile": "level1"}
+                    ],
                 }
             ],
-            'items': []
+            "items": [],
         }
 
         for canvas in obj.canvas_set.all():
-            data['items'].append(
+            data["items"].append(
                 json.loads(
-                    serialize(
-                        'canvas_v3',
-                        [canvas],
-                        current_user=self.current_user
-                    )
+                    serialize("canvas_v3", [canvas], current_user=self.current_user)
                 )
             )
 
         return data
+
+
+# class Deserializer:
+#     """
+#     Deserialize IIIF v3 Manifest
+#     """

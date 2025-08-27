170336bf0cb23e13b1b7da848f347d98c4219a5c
Y <yl5682@icloud.com>
2025-08-27 15:15:10 -0400

    update external image download link

--- apps/static/js/index.js
+++ apps/static/js/index.js
@@ -6,3 +6,4 @@ window.UIkit = require('uikit');
 window.UIkitIcons = require('/apps/static/js/uikit-icons.min.js');
 // require('/apps/static/js/project.js');
 window.ECDSAnnotator = require('../../../node_modules/ecds-annotator/dist/ecds-annotator.min.js');
+require('/apps/static/js/custom.js');

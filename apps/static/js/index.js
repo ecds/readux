d393f5d487ccfeaf3fad6eff4e5c500c58aea05f
Y <yl5682@icloud.com>
2025-09-16 01:09:00 -0400

    add necessary uikit-icons

--- apps/static/js/index.js
+++ apps/static/js/index.js
@@ -5,7 +5,7 @@ require('/apps/static/js/vue-readux.js');
 
 // UIkit
 window.UIkit = require('uikit');
-
+window.UIkitIcons = require('uikit/dist/js/uikit-icons');
 // require('/apps/static/js/project.js');
 window.ECDSAnnotator = require('../../../node_modules/ecds-annotator/dist/ecds-annotator.min.js');
 // require('/apps/static/js/custom.js');

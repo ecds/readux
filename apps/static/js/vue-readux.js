ff6875bd78fe801f90d8b94b4b13783f58223516
Y <yl5682@icloud.com>
2025-09-16 00:51:30 -0400

    remove sort_dropdown.html and related javascript

--- apps/static/js/vue-readux.js
+++ apps/static/js/vue-readux.js
@@ -653,38 +653,6 @@ var readux = new Vue({
     toggleMoreInfo: function () {
       this.showMoreInfo = !this.showMoreInfo
     }
-
-    // ascaddURL: function(element) {
-    // 	$(element).attr('href', function () {
-    // 		if (window.location.search.length == 0) {
-    // 			return this.href + '?sort=title&order=asc';
-    // 		} else if (window.location.search.match(/[?&]order=asc/gi)) {
-    // 			this.href = this.href.replace('order=asc', 'order=asc');
-    // 			return this.href;
-    // 		} else if (window.location.search.match(/[?&]order=desc/gi)) {
-    // 			this.href = this.href.replace('order=desc', 'order=asc');
-    // 			return this.href;
-    // 		} else {
-    // 			return this.href + '&order=asc';
-    // 		}
-    // 	});
-    // },
-
-    // descaddURL: function(element) {
-    // 	$(element).attr('href', function () {
-    // 		if (window.location.search.length == 0) {
-    // 			return this.href + '?sort=title&order=desc';
-    // 		} else if (window.location.search.match(/[?&]order=asc/gi)) {
-    // 			this.href = this.href.replace('order=asc', 'order=desc');
-    // 			return this.href;
-    // 		} else if (window.location.search.match(/[?&]order=desc/gi)) {
-    // 			this.href = this.href.replace('order=desc', 'order=desc');
-    // 			return this.href;
-    // 		} else {
-    // 			return this.href + '&order=desc';
-    // 		}
-    // 	});
-    // }
   },
 
   mounted: function () {

685ede9f531dc540f2829760c7eff788899fdf19
Y <yl5682@icloud.com>
2025-09-19 20:04:21 -0400

    Revert "overhaul vue (not working)"
    This reverts commit 7a50e1eb2e64769a2e15361c67c15d5a6ffeaff4.

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,24 @@
 const path = require("path");
 const webpack = require("webpack");
 const BundleTracker = require("webpack-bundle-tracker");
-const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
   context: __dirname,
   mode: "production",
-  entry: "./apps/static/js/index.js",
+  entry: "./apps/static/js/index",
   watch: false,
   output: {
     path: path.resolve("./apps/static/js/"),
     filename: "[name].js",
   },
-  resolve: {
-    extensions: ['.js', '.vue', '.json'],
-    alias: {
-      // Use the template-enabled build of Vue 2
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': path.resolve(__dirname, 'apps/static/js'),
-    },
-  },
   devtool: "source-map",
-  plugins: [new BundleTracker({ filename: "./webpack-stats.json" }), new VueLoaderPlugin()],
+  plugins: [new BundleTracker({ filename: "./webpack-stats.json" })],
   module: {
     rules: [
-      // Compile .vue Single File Components
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-      },
       {
         test: /\.js$/,
         enforce: "pre",
         use: ["source-map-loader"],
-        exclude: [/node_modules\/ecds-annotator/],
       },
       {
         test: /\.css$/i,
